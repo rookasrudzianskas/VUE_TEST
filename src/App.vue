1ce67dff1d4ff37a1408cea5cbee3ca8d5034188
Rokas <rookas.rudzenskas@gmail.com>
2021-05-14 12:12:13 +0300

    Rokas

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Options API</router-link> |
+    <router-link to="/composition">Composition API</router-link>
   </div>
   <router-view/>
 </template>
--- src/views/Options.vue
+++ src/views/Options.vue
@@ -1,18 +1,73 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>You have XXX todos</h3>
+    <div>
+      <input type="text" placeholder="Add a todo">
+    </div>
+
+    <div>
+      <ul>
+        <li>
+          <span>One</span>
+          <button>X</button>
+        </li>
+
+        <li>
+          <span>Two</span>
+          <button>X</button>
+        </li>
+
+        <li>
+          <span>Three</span>
+          <button>X</button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          name: 'One',
+        },
+
+        {
+          id: 2,
+          name: 'Two',
+        },
+
+        {
+          id: 3,
+          name: 'Three',
+        },
+      ]
+    }
   }
 }
 </script>
+
+<style>
+ul {
+  list-style: none;
+  padding: 0;
+  width: 200px;
+  margin: 20px auto 0;
+}
+
+li {
+  border: 1px solid;
+  display: flex;
+  margin-bottom: 10px;
+}
+
+li span {
+  flex-grow: 1;
+}
+</style>
